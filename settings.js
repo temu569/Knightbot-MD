71fcec7028aad3fb2b207ec05c578002d1f7b7df
temu569 <thembinkosichipere@gmail.com>
2025-04-16 22:21:38 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Temptation', // Your name
+  ownerNumber: '+263788516796', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Thfor'Temptationn 'Temptationn managing group commands and automating tasks.",
   version: "1.0.0",
 };
 
